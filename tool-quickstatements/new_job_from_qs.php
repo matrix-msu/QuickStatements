0e49a0cd2e9171b4433b1bcee1f593f4ae4d2c58
Josh Christ <josh.christ@peach.matrix.msu.edu>
2019-05-23 17:47:56 -0400

    adding tool-quickstatements

---
+++ tool-quickstatements/new_job_from_qs.php
@@ -0,0 +1,24 @@
+#!/usr/bin/php
+<?PHP
+
+#error_reporting(E_ERROR|E_CORE_ERROR|E_ALL|E_COMPILE_ERROR);
+$user_id_magnus = 4420 ;
+
+require_once ( __DIR__ . '/public_html/quickstatements.php' ) ;
+
+if ( !isset($argv[1]) ) die ( "Requires QS file as parameter\n" ) ;
+$fn = $argv[1] ;
+if ( !file_exists($fn) ) die ( "File $fn does not exist\n" ) ;
+$commands = file_get_contents ( $fn ) ;
+
+$batch_name = isset($argv[2]) ? $argv[2] : '' ;
+
+$qs = new QuickStatements ;
+$qs->use_command_compression = true ;
+$j = $qs->importData ( $commands , 'V1' , true ) ;
+#$j['data']['commands'] = $qs->compressCommands ( $j['data']['commands'] ) ;
+#print_r ( $j ) ;
+$batch_id = $qs->addBatch ( $j['data']['commands'] , $user_id_magnus , $batch_name ) ;
+print "Now as batch #$batch_id with " . count($j['data']['commands']) . " commands\n" ;
+
+?>
---
+++ tool-quickstatements/public_html/vue.js
@@ -0,0 +1,53 @@
+'use strict';
+
+let router ;
+let app ;
+let wd = new WikiData() ;
+
+let config = {} ;
+let prop_map = {} ;
+let working = false ;
+
+$(document).ready ( function () {
+    vue_components.toolname = 'quickstatements' ;
+//    vue_components.components_base_url = 'https://tools.wmflabs.org/magnustools/resources/vue/' ; // For testing; turn off to use tools-static
+    Promise.all ( [
+            vue_components.loadComponents ( ['wd-date','wd-link','tool-translate','tool-navbar','commons-thumbnail',
+                'vue_components/batch_access_mixin.html',
+                'vue_components/user.html',
+                'vue_components/main-page.html',
+                'vue_components/command.html',
+                'vue_components/batch-commands.html',
+                'vue_components/batch.html',
+                'vue_components/batches.html',
+                'vue_components/user-page.html',
+                ] ) ,
+            new Promise(function(resolve, reject) {
+                $.get ( './config.json' , function (d) {
+                    config = d ;
+                    resolve() ;
+                } , 'json' ) ;
+            } )
+    ] ) .then ( () => {
+        wd_link_base = config.sites[config.site].pageBase ;
+        wd_link_wd = wd ;
+        wd.api = config.sites[config.site].api + '?callback=?' ;
+        wd_ns_prefixes = {} ;
+        for ( var letter in config.sites[config.site].types )
+            wd_ns_prefixes[letter] = config.sites[config.site].types[letter].ns_prefix ;
+
+        const routes = [
+          { path: '/', component: MainPage , props:true },
+          { path: '/batches', component: BatchesPage , props:true },
+          { path: '/batches/:user_name', component: BatchesPage , props:true },
+          { path: '/batch', component: BatchPage , props:true },
+          { path: '/batch/:batch', component: BatchPage , props:true },
+          { path: '/user', component: UserPage , props:true },
+          { path: '/user/:given_user_name', component: UserPage , props:true },
+          { path: '/:url_params', component: MainPage , props:true },
+        ] ;
+        router = new VueRouter({routes}) ;
+        app = new Vue ( { router } ) .$mount('#app') ;
+    } ) ;
+} ) ;
+
